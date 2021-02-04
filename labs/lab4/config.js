d9320a1db81d642937c61eef6b3d1044bccd7f41
Cvai <cvaikun.com@gmail.com>
2021-02-05 00:11:34 +0200

    test

--- labs/lab4/config.js
+++ labs/lab4/config.js
@@ -1,3 +1,4 @@
 require('dotenv').config();
 
-module.exports = process.env.MONGODB_URL;
+// module.exports = process.env.MONGODB_URL;
+module.exports = 'mongodb+srv://qwerty:qwerty123@cluster0.wjoar.mongodb.net/webprogbase?retryWrites=true&w=majority';
