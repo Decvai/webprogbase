6957dadfcd39e2d3053f1329d8834b95c855acad
Cvai <cvaikun.com@gmail.com>
2021-02-05 00:40:30 +0200

    set heroku env vars

--- labs/lab4/config.js
+++ labs/lab4/config.js
@@ -1,4 +1,3 @@
 require('dotenv').config();
 
-// module.exports = process.env.MONGODB_URL;
-module.exports = 'mongodb+srv://qwerty:qwerty123@cluster0.wjoar.mongodb.net/webprogbase?retryWrites=true&w=majority';
+module.exports = process.env.MONGODB_URL;
